ca24aa1c60ae6b0c0a4ab818649a01b8bd58cb59
rrbrink <ryanbrinkerhoff@live.com>
2017-11-08 15:33:52 -0700

    first commit

---
+++ frontPage/script.js
@@ -0,0 +1,9 @@
+
+
+
+$("#subnitBtn").on("click", function(){
+var usernameImput = $("#usrName").val();
+var passwordImput = $("#usrPass").val().trim();
+console.log(passwordImput);
+alert("hello")
+});
